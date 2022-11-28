5f526413f72f6bf1dc662b514fc98d5851a118f5
tharunkumarbottlapally <117142625+tharunkumarbottlapally@users.noreply.github.com>
2022-11-28 18:08:05 -0500

    Add files via upload

---
+++ pages/01_Batting.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.markdown("# Batting 🏏")
+st.sidebar.markdown("# Batting 🏏")
+#tab1, tab2 = st.tabs(['Batting','Bowling'])
---
+++ pages/02_Bowling.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.markdown("# Bowling 🏏")
+st.sidebar.markdown("# Bowling 🏏")

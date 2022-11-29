e6fbfebf6a8639427de00be20dcd74ba96550635
tharunkumarbottlapally <117142625+tharunkumarbottlapally@users.noreply.github.com>
2022-11-28 19:57:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -10,15 +10,17 @@ st.sidebar.markdown("# IPL - Player Performance Dataset")
 st.subheader('Content')
 st.text(' The Indian Premier League (IPL) is a professional mens Twenty20 cricket league, contested by ten teams based out of ten Indian cities.\n The league was founded by the Board of Control for Cricket in India (BCCI) in 2007.\n \n It is usually held between March and May of every year and has an exclusive window in the ICC Future Tours Programme.\n The IPL is the most-attended cricket league in the world and in 2014 was ranked sixth by average attendance among all sports leagues.\n In 2010, the IPL became the first sporting event in the world to be broadcast live on YouTube.\n The brand value of the IPL in 2019 was ₹47,500 crores (6.3 billion US dollars), according to Duff & Phelps.\n According to BCCI, the 2015 IPL season contributed ₹1,150 crores (150 million US dollars) to the GDP of the Indian economy.\n \n The 2020 IPL season set a massive viewership record with 31.57 million average impressions and with an overall consumption increase of 23 percent from the 2019 season.\n There have been fourteen seasons of the IPL tournament.\n The current IPL title holders are the Chennai Super Kings, winning the 2021 season.\n \n The venue for the 2020 season was moved due to the COVID-19 pandemic and games were played in the United Arab Emirates.')
 
-#bowlingtab,battingtab = st.tabs(['Bowling','Batting'])
-page = st.sidebar.selectbox('Select Stats',
-  ['Bowling','Batting'])
+bowlingtab,battingtab = st.tabs(['Bowling','Batting'])
+#page = st.sidebar.selectbox('Select Stats',
+ # ['Bowling','Batting'])
 
 
 
 ###### BestBowlingEconomy ###########
 
-if page == 'Bowling':
+#if page == 'Bowling':
+with bowlingtab:
+
     tab1, tab2,tab3,tab4,tab5 = st.tabs(['Best Bowling Economy','Best Bowling Strike',
                             'Most Dot Balls Inning','Most Runs Conceded Innings','Most Wickets',])
     with tab1:
@@ -97,7 +99,8 @@ if page == 'Bowling':
                         )
         st.altair_chart(BarMostWickets,use_container_width=True)     
 ####### BATTING ###########
-if page == 'Batting':
+#if page == 'Batting':
+with battingtab:
     tabbat1,tabbat2,tabbat3,tabbat4,tabbat5=st.tabs(['Most Runs','Most Sixes Innings','Most Fours Innings','Fastest Centuries','Fastest Fifties'])
     with tabbat1:
                         ###### Most Runs ###########

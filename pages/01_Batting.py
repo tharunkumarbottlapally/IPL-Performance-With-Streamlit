1c069d2f1a9508d87723ca085065a84ea331eaf5
tharunkumarbottlapally <117142625+tharunkumarbottlapally@users.noreply.github.com>
2022-12-11 17:38:35 -0500

    Update 01_Batting.py

--- pages/01_Batting.py
+++ pages/01_Batting.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import altair as alt
 
-# Creating a dictionary not a list 
 
 alltime,seasons=st.tabs(['All Time Records','By Season Stats'])
 with alltime:
@@ -14,8 +13,9 @@ with alltime:
                 MostRuns = pd.read_csv("IPLDataset/AllSeasonsCombined/Most Runs All Seasons Combine.csv")
                 MostRuns=MostRuns.sort_values(by=['Runs'],ascending=False)
                 st.write('## Most Runs ')
+                MostRuns=MostRuns.reset_index(drop=True)
                 st.write(MostRuns)
-                top20MostRuns=MostRuns.head(20)
+                top20MostRuns=MostRuns.head(100)
 
                 BarMostRuns=alt.Chart(top20MostRuns).mark_bar().encode(
                                 x='Runs',
@@ -24,8 +24,8 @@ with alltime:
                                 color='Player',
                                 order=alt.Order(
                                 # Sort the segments of the bars by this field
-                                'Runs',
-                                sort='descending'
+                                'Player',
+                                sort='ascending'
                                 )
                                 )
                 st.altair_chart(BarMostRuns,use_container_width=True)
@@ -35,14 +35,21 @@ with alltime:
                 MostSixesInnings = pd.read_csv("IPLDataset/AllSeasonsCombined/Most Sixes Per Innings All Seasons Combine.csv")
                 MostSixesInnings=MostSixesInnings.sort_values(by=['6s'],ascending=False)
                 st.write('## Most Sixes Innings ')
+                MostSixesInnings=MostSixesInnings.reset_index(drop=True)
                 st.write(MostSixesInnings)
+                top20MostSixesInnings=MostSixesInnings.style.hide_index()
                 top20MostSixesInnings=MostSixesInnings.head(20)
 
                 BarMostSixesInnings=alt.Chart(top20MostSixesInnings).mark_bar().encode(
-                                x=alt.Y('6s', sort='-x'),
+                                x=alt.Y('6s'),
                                 y=alt.Y('Player', sort='-x'),
                                 tooltip=['6s'],
                                 color='Against',
+                                 order=alt.Order(
+                                # Sort the segments of the bars by this field
+                                'Player',
+                                sort='descending'
+                                )
                                 
                                 )
                 st.altair_chart(BarMostSixesInnings,use_container_width=True)
@@ -52,14 +59,19 @@ with alltime:
                 MostFoursInnings = pd.read_csv("IPLDataset/AllSeasonsCombined/Most Fours Per Innings All Seasons Combine.csv")
                 MostFoursInnings=MostFoursInnings.sort_values(by=['4s'],ascending=False)
                 st.write('## Most Fours Innings ')
+                MostFoursInnings=MostFoursInnings.reset_index(drop=True)
                 st.write(MostFoursInnings)
-                top20MostFoursInnings=MostFoursInnings.head(20)
+                top20MostFoursInnings=MostFoursInnings.head(50)
 
                 BarMostFoursInnings=alt.Chart(top20MostFoursInnings).mark_bar().encode(
                                 x='4s',
                                 y=alt.Y('Player', sort='-x'),
                                 tooltip=['4s'],
-                                color='Against',
+                                color='Against',order=alt.Order(
+                                # Sort the segments of the bars by this field
+                                'Player',
+                                sort='descending'
+                                )
                                 
                                 )
                 st.altair_chart(BarMostFoursInnings,use_container_width=True)
@@ -69,6 +81,7 @@ with alltime:
                 FastestCenturies = pd.read_csv("IPLDataset/AllSeasonsCombined/Fastest Centuries All Seasons Combine.csv")
                 FastestCenturies=FastestCenturies.sort_values(by=['BF'],ascending=True)
                 st.write('## Fastest Centuries')
+                FastestCenturies=FastestCenturies.reset_index(drop=True)
                 st.write(FastestCenturies)
                 top20FastestCenturies=FastestCenturies.head(20)
 
@@ -86,6 +99,7 @@ with alltime:
                 FastestFifties = pd.read_csv("IPLDataset/AllSeasonsCombined/Fastest Fifties All Seasons Combine.csv")
                 FastestFifties=FastestFifties.sort_values(by=['BF'],ascending=True)
                 st.write('## Fastest Centuries')
+                FastestFifties=FastestFifties.reset_index(drop=True)
                 st.write(FastestFifties)
                 top20FastestFifties=FastestFifties.head(20)
 
@@ -102,6 +116,7 @@ with alltime:
                         MostRunsover = pd.read_csv("IPLDataset/AllSeasonsCombined/Most Runs Per Over All Seasons Combine.csv")
                         MostRunsover=MostRunsover.sort_values(by=['Runs'],ascending=False)
                         st.write('## Most Runs over')
+                        MostRunsover=MostRunsover.reset_index(drop=True)
                         st.write(MostRunsover)
                         top20MostRunsover=MostRunsover.head(20)
 
@@ -121,108 +136,114 @@ with seasons:
         if options ==options:
                 tabbat1,tabbat2,tabbat3,tabbat4,tabbat5,tabbat6=st.tabs(['Most Runs','Most Sixes Innings','Most Fours Innings','Fastest Centuries','Fastest Fifties','Most Runs Over'])
                 with tabbat1:
-                                        ###### Most Runs ###########
-                                MostRuns = pd.read_csv("IPLDataset/MostRuns/Most Runs - "+options+".csv")
-                                MostRuns=MostRuns.sort_values(by=['Runs'],ascending=False)
-                                st.write('## Most Runs ')
-                                st.write(MostRuns)
-                                top20MostRuns=MostRuns.head(20)
+                                ###### Most Runs ###########
+                        MostRuns = pd.read_csv("IPLDataset/MostRuns/Most Runs - "+options+".csv")
+                        MostRuns=MostRuns.sort_values(by=['Runs'],ascending=False)
+                        st.write('## Most Runs ')
+                        MostRuns=MostRuns.reset_index(drop=True)
+                        st.write(MostRuns)
+                        top20MostRuns=MostRuns.head(20)
 
-                                BarMostRuns=alt.Chart(top20MostRuns).mark_bar().encode(
-                                                x='Runs',
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['Runs'],
-                                                color='Player',
-                                                order=alt.Order(
-                                                # Sort the segments of the bars by this field
-                                                'Runs',
-                                                sort='descending'
-                                                )
-                                                )
-                                st.altair_chart(BarMostRuns,use_container_width=True)
+                        BarMostRuns=alt.Chart(top20MostRuns).mark_bar().encode(
+                                        x='Runs',
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['Runs'],
+                                        color='Player',
+                                        order=alt.Order(
+                                        # Sort the segments of the bars by this field
+                                        'Runs',
+                                        sort='descending'
+                                        )
+                                        )
+                        st.altair_chart(BarMostRuns,use_container_width=True)
 
                 with tabbat2:
-                                        ###### Most Sixes Innings ###########
-                                MostSixesInnings = pd.read_csv("IPLDataset/MostSixesInnings/Most Sixes Innings - "+options+".csv")
-                                MostSixesInnings=MostSixesInnings.sort_values(by=['6s'],ascending=False)
-                                st.write('## Most Sixes Innings ')
-                                st.write(MostSixesInnings)
-                                top20MostSixesInnings=MostSixesInnings.head(20)
+                                ###### Most Sixes Innings ###########
+                        MostSixesInnings = pd.read_csv("IPLDataset/MostSixesInnings/Most Sixes Innings - "+options+".csv")
+                        MostSixesInnings=MostSixesInnings.sort_values(by=['6s'],ascending=False)
+                        st.write('## Most Sixes Innings ')
+                        MostSixesInnings=MostSixesInnings.reset_index(drop=True)
+                        st.write(MostSixesInnings)
+                        top20MostSixesInnings=MostSixesInnings.head(20)
 
-                                BarMostSixesInnings=alt.Chart(top20MostSixesInnings).mark_bar().encode(
-                                                x=alt.Y('6s', sort='-x'),
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['6s'],
-                                                color='Against',
-                                                
-                                                )
-                                st.altair_chart(BarMostSixesInnings,use_container_width=True)
+                        BarMostSixesInnings=alt.Chart(top20MostSixesInnings).mark_bar().encode(
+                                        x=alt.Y('6s', sort='-x'),
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['6s'],
+                                        color='Against',
+                                        
+                                        )
+                        st.altair_chart(BarMostSixesInnings,use_container_width=True)
 
                 with tabbat3:
-                                        ###### Most Fours Innings ###########
-                                MostFoursInnings = pd.read_csv("IPLDataset/MostFoursInnings/Most Fours Innings - "+options+".csv")
-                                MostFoursInnings=MostFoursInnings.sort_values(by=['4s'],ascending=False)
-                                st.write('## Most Fours Innings ')
-                                st.write(MostFoursInnings)
-                                top20MostFoursInnings=MostFoursInnings.head(20)
+                                ###### Most Fours Innings ###########
+                        MostFoursInnings = pd.read_csv("IPLDataset/MostFoursInnings/Most Fours Innings - "+options+".csv")
+                        MostFoursInnings=MostFoursInnings.sort_values(by=['4s'],ascending=False)
+                        st.write('## Most Fours Innings ')
+                        MostFoursInnings=MostFoursInnings.reset_index(drop=True)
+                        st.write(MostFoursInnings)
+                        top20MostFoursInnings=MostFoursInnings.head(20)
 
-                                BarMostFoursInnings=alt.Chart(top20MostFoursInnings).mark_bar().encode(
-                                                x='4s',
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['4s'],
-                                                color='Against',
-                                                
-                                                )
-                                st.altair_chart(BarMostFoursInnings,use_container_width=True)
+                        BarMostFoursInnings=alt.Chart(top20MostFoursInnings).mark_bar().encode(
+                                        x='4s',
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['4s'],
+                                        color='Against',
+                                        
+                                        )
+                        st.altair_chart(BarMostFoursInnings,use_container_width=True)
 
                 with tabbat4:
-                                        ###### Fastest Centuries ###########
-                                FastestCenturies = pd.read_csv("IPLDataset/FastestCenturies/Fastest Centuries - "+options+".csv")
-                                FastestCenturies=FastestCenturies.sort_values(by=['BF'],ascending=True)
-                                st.write('## Fastest Centuries')
-                                st.write(FastestCenturies)
-                                top20FastestCenturies=FastestCenturies.head(20)
+                                ###### Fastest Centuries ###########
+                        FastestCenturies = pd.read_csv("IPLDataset/FastestCenturies/Fastest Centuries - "+options+".csv")
+                        FastestCenturies=FastestCenturies.sort_values(by=['BF'],ascending=True)
+                        st.write('## Fastest Centuries')
+                        FastestCenturies=FastestCenturies.reset_index(drop=True)
+                        st.write(FastestCenturies)
+                        top20FastestCenturies=FastestCenturies.head(20)
 
-                                BarFastestCenturies=alt.Chart(top20FastestCenturies).mark_bar().encode(
-                                                x='Runs',
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['BF'],
-                                                color='Against',
+                        BarFastestCenturies=alt.Chart(top20FastestCenturies).mark_bar().encode(
+                                        x='Runs',
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['BF'],
+                                        color='Against',
 
-                                                )
-                                st.altair_chart(BarFastestCenturies,use_container_width=True)
+                                        )
+                        st.altair_chart(BarFastestCenturies,use_container_width=True)
 
                 with tabbat5:
-                                        ###### Fastest Fifties ###########
-                                FastestFifties = pd.read_csv("IPLDataset/FastestFifties/Fastest Fifties - "+options+".csv")
-                                FastestFifties=FastestFifties.sort_values(by=['BF'],ascending=True)
-                                st.write('## Fastest Centuries')
-                                st.write(FastestFifties)
-                                top20FastestFifties=FastestFifties.head(20)
+                                ###### Fastest Fifties ###########
+                        FastestFifties = pd.read_csv("IPLDataset/FastestFifties/Fastest Fifties - "+options+".csv")
+                        FastestFifties=FastestFifties.sort_values(by=['BF'],ascending=True)
+                        st.write('## Fastest Centuries')
+                        FastestFifties=FastestFifties.reset_index(drop=True)
+                        st.write(FastestFifties)
+                        top20FastestFifties=FastestFifties.head(20)
 
-                                BarFastestFifties=alt.Chart(top20FastestFifties).mark_bar().encode(
-                                                x='Runs',
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['BF'],
-                                                color='Against',
-                                                
-                                                )
-                                st.altair_chart(BarFastestFifties,use_container_width=True)
+                        BarFastestFifties=alt.Chart(top20FastestFifties).mark_bar().encode(
+                                        x='Runs',
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['BF'],
+                                        color='Against',
+                                        
+                                        )
+                        st.altair_chart(BarFastestFifties,use_container_width=True)
                 with tabbat6:
-                                        ###### Most Runs over ###########
-                                MostRunsover = pd.read_csv("IPLDataset/MostRunsOver/Most Runs Over - "+options+".csv")
-                                MostRunsover=MostRunsover.sort_values(by=['Runs'],ascending=False)
-                                st.write('## Most Runs over')
-                                st.write(MostRunsover)
-                                top20MostRunsover=MostRunsover.head(20)
+                                ###### Most Runs over ###########
+                        MostRunsover = pd.read_csv("IPLDataset/MostRunsOver/Most Runs Over - "+options+".csv")
+                        MostRunsover=MostRunsover.sort_values(by=['Runs'],ascending=False)
+                        st.write('## Most Runs over')
+                        MostRunsover=MostRunsover.reset_index(drop=True)
+                        st.write(MostRunsover)
+                        top20MostRunsover=MostRunsover.head(20)
 
-                                BarMostRunsover=alt.Chart(top20MostRunsover).mark_bar().encode(
-                                                x='Runs',
-                                                y=alt.Y('Player', sort='-x'),
-                                                tooltip=['Runs','Match Date'],
-                                                color='Against',
-                                                
-                                                )
-                                st.altair_chart(BarMostRunsover,use_container_width=True)
+                        BarMostRunsover=alt.Chart(top20MostRunsover).mark_bar().encode(
+                                        x='Runs',
+                                        y=alt.Y('Player', sort='-x'),
+                                        tooltip=['Runs','Match Date'],
+                                        color='Against',
+                                        
+                                        )
+                        st.altair_chart(BarMostRunsover,use_container_width=True)
 
 
